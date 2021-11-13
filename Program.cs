90f12107b44de3f439da4fa113434fdfef817483
olmosmartin <tiincho18@gmail.com>
2021-11-13 15:43:05 -0300

    CRUD sobre base de datos en mongo db

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
 using Catalog.Repositories;
+using MongoDB.Driver;
+using Catalog.Settings;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 
-//add singleton
-builder.Services.AddSingleton<IItemsEnMemoria, ItemsEnMemoria>();
+// usar guid con otra serializacion mas entendible
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+//usar datetime con otra serializacion mas entendible
+BsonSerializer.RegisterSerializer(new DateTimeSerializer(BsonType.String));
+
+//add singleton mongo client
+builder.Services.AddSingleton<IMongoClient>(serviceProvider=>{
+    var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+    return new MongoClient(settings.ConnectionString);
+});
+//add singleton para usar el guardado en memoria
+//builder.Services.AddSingleton<IItemsEnMemoria, ItemsEnMemoria>();
+//singleto para usar mongo
+builder.Services.AddSingleton<IItemsEnMemoria, MongoDbItemRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
